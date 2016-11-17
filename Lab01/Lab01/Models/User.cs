8ee0dc3604a3428a43861acddb4586b035f5a459
Ivan Prgomet <ivan_prgomet@hotmail.com>
2016-11-17 12:18:43 +0100

    added user model, and user controller by template. no views for user added yet

---
+++ Lab01/Lab01/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lab01.Models
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public DateTime DateRegistered { get; set; }
+    }
+}
