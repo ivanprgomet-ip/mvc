2850c4b1d790b699b587e94b1c402c412f229b83
Ivan Prgomet <ivan_prgomet@hotmail.com>
2016-11-18 10:18:22 +0100

    added simple login logic, but not using areas yet

--- Lab01/Lab01/Controllers/AuthorizationController.cs
+++ Lab01/Lab01/Controllers/AuthorizationController.cs
@@ -11,14 +11,47 @@ namespace Lab01.Controllers
     {
         public static List<User> _usersDB = new List<User>();
 
-        // login and logout 
+        
+
+        /// <summary>
+        /// provides the username with a login screen
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
         public ActionResult Login()
         {
             return View();
         }
+
+        /// <summary>
+        /// recieves the username and password back after
+        /// user submits the form
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost]
+        public ActionResult Login(User user)//could also pass in user object, mvc smart enough to place username and password into object
+        {
+            //retrieve the user if he exists
+            if (UserExists(user))
+                return Content("Successfull Login");
+            else
+                return Content("Login failed");
+        }
+
+
         public ActionResult Logout()
         {
             return View();
+        }
+
+        private bool UserExists(User userToBeLoggedIn)
+        {
+            foreach (var existingUser in _usersDB)
+            {
+                if (existingUser.Username == userToBeLoggedIn.Username && existingUser.Password == userToBeLoggedIn.Password)
+                    return true;
+            }
+            return false;
         }
     }
 }
