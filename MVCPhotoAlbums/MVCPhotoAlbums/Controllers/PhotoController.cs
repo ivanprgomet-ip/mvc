f004ee374be21bf833d90ec765fdbb53a28876d1
Ivan Prgomet <ivan_prgomet@hotmail.com>
2016-11-22 19:36:07 +0100

    upload photos to existing album

---
+++ MVCPhotoAlbums/MVCPhotoAlbums/Controllers/PhotoController.cs
@@ -0,0 +1,44 @@
+﻿using MVCPhotoAlbums.Models;
+using MVCPhotoAlbums.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCPhotoAlbums.Controllers
+{
+    public class PhotoController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(PhotoModel photo, HttpPostedFileBase[] filesToBeUploaded)
+        {
+            //retrieving the album id through the photo id, and then the username through album property 'user'
+            AlbumRepository albumRepo = new AlbumRepository();
+            Guid albumId = photo.Id; //why is the photo id and album id is the same?
+            var album = albumRepo.ReturnAlbum(albumId);
+
+            //create the photo
+            PhotoRepository repo = new PhotoRepository();
+            repo.CreatePhoto(photo);
+
+            foreach (var file in filesToBeUploaded)
+            {
+                file.SaveAs(Server.MapPath("~/Content/Albums/"+ album.User.Username + "/"+album.Name+"/"+file.FileName));
+            };
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ MVCPhotoAlbums/MVCPhotoAlbums/Repositories/PhotoRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MVCPhotoAlbums.Models;
+
+namespace MVCPhotoAlbums.Repositories
+{
+    public class PhotoRepository
+    {
+        internal void CreatePhoto(PhotoModel photo)
+        {
+            photo.DateCreated = DateTime.Now;
+            photo.Id = Guid.NewGuid();
+        }
+    }
+}
