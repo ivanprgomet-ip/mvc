a3b80e386656437acd3f23d7c283200629ecd7ee
Ivan Prgomet <ivan_prgomet@hotmail.com>
2016-11-28 18:52:52 +0100

    shared partial view for listing photos

--- Mvc/MvcLab.Web/scripts/photo.create.js
+++ Mvc/MvcLab.Web/scripts/photo.create.js
@@ -1,39 +1,23 @@
 ﻿/// <reference path="jquery-3.1.1.intellisense.js" />
 
-//AJAX is about loading data in the background and display it on the webpage, without reloading the whole page
 
-//when the document has finnished rendering..
-$(document).ready(function () {
+//$(document).ready(function () {
 
-    //find and retrieve the current form on the page..
-    var form = $("form_photo_create");
+//    $(document).ajaxStart(function () {
+//        //show a progress modal of your choosing
+//        showProgressModal('loading');
+//    });
+//    $(document).ajaxStop(function () {
+//        //hide the progress modal 
+//        hideProgressModal();
+//    });
 
-    //and start listening for what happens when the form gets submitted..
-    form.submit(function (e) {
+//    $.ajax({
+//        url: '/Photo/Create',
+//        dataType: 'html',
+//        success: function (data) {
+//            $('#myPartialContainer').html(data);
+//        }
+//    });
 
-        //dont send the form automatically!
-        e.preventDefault();
-
-        //upload the file (photo) and the form with the help of ajax!
-        $.ajax({
-
-            //we are making a post, because the controller action is attributed with the post attribute
-            method: "Post",
-
-            url: "/Photo/Create",
-
-            data: new FormData(document.getElementsByTagName("Form")[0]),
-
-            //when our call is done, and we get an answer
-            //then we run the following function
-            success: function (data) {
-
-                $("div#result").html(data)
-            },
-
-            //these are required for us to use formdata!
-            processData: false,
-            contentType: false
-        });
-    });
-});
+//});
