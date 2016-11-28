5813155bc7d8334a35e1effb0b355abba26f43e1
Ivan Prgomet <ivan_prgomet@hotmail.com>
2016-11-28 08:58:09 +0100

    testing ajax

---
+++ Mvc/MvcLab.Web/scripts/photo.create.js
@@ -0,0 +1,19 @@
+﻿/// <reference path="jquery-1.10.2.intellisense.js" />
+
+//ajax for uploading a photo
+
+(function () {
+    //get the photo create form
+    var form = document.getElementById("form_photo_create");
+
+    form.addEventListener("Submit", function (e) {
+        e.preventDefault();
+
+        //get the photo description textbox value of the form
+        var photoDescription = document.getElementById("photo_create_description");
+
+        //alert the description just submitted
+        alert(photoDescription);
+    });
+
+})();
