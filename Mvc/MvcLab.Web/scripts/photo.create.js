930849d0d94346831176b858aaf5c4deee684fe6
Ivan Prgomet <ivan_prgomet@hotmail.com>
2016-11-28 17:53:18 +0100

    did the addressbook exercise

--- Mvc/MvcLab.Web/scripts/photo.create.js
+++ Mvc/MvcLab.Web/scripts/photo.create.js
@@ -1,19 +1,39 @@
-﻿/// <reference path="jquery-1.10.2.intellisense.js" />
+﻿/// <reference path="jquery-3.1.1.intellisense.js" />
 
-//ajax for uploading a photo
+//AJAX is about loading data in the background and display it on the webpage, without reloading the whole page
 
-(function () {
-    //get the photo create form
-    var form = document.getElementById("form_photo_create");
+//when the document has finnished rendering..
+$(document).ready(function () {
 
-    form.addEventListener("Submit", function (e) {
+    //find and retrieve the current form on the page..
+    var form = $("form_photo_create");
+
+    //and start listening for what happens when the form gets submitted..
+    form.submit(function (e) {
+
+        //dont send the form automatically!
         e.preventDefault();
 
-        //get the photo description textbox value of the form
-        var photoDescription = document.getElementById("photo_create_description");
+        //upload the file (photo) and the form with the help of ajax!
+        $.ajax({
 
-        //alert the description just submitted
-        alert(photoDescription);
+            //we are making a post, because the controller action is attributed with the post attribute
+            method: "Post",
+
+            url: "/Photo/Create",
+
+            data: new FormData(document.getElementsByTagName("Form")[0]),
+
+            //when our call is done, and we get an answer
+            //then we run the following function
+            success: function (data) {
+
+                $("div#result").html(data)
+            },
+
+            //these are required for us to use formdata!
+            processData: false,
+            contentType: false
+        });
     });
-
-})();
+});
