1756f165f40e75287cf20cc49ac58acce4ffb8d5
Ivan Prgomet <ivan_prgomet@hotmail.com>
2016-11-30 18:02:22 +0100

    initialized some very basic testing methods

---
+++ Mvc/MvcLab.Tests/UserControllerTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MvcLab.Web.Controllers;
+using System.Web;
+using System.Web.Routing;
+using System.Web.Mvc;
+
+namespace MvcLab.Tests
+{
+    [TestClass]
+    public class UserControllerTests
+    {
+        public void SomeTest()
+        {
+            // ARRANGE
+            var controller = new UserController();
+            var httpRequest = new HttpRequest("", "http://localhost/User/", "");
+            var httpContext = new HttpContext(httpRequest, new HttpResponse(null));
+            var requestContext = new RequestContext(new HttpContextWrapper(httpContext), new RouteData());
+            controller.ControllerContext = new ControllerContext(requestContext, controller);
+
+            // ACT
+            var index = controller.Index() as ViewResult;
+
+            // ASSERT
+            Assert.IsNotNull(index);
+            Assert.AreEqual("/User/", index.ViewBag.RawUrl);
+        }
+    }
+}
---
+++ Mvc/MvcLab.Tests/UserTests.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MvcLab.Data.Models;
+using MvcLab.Data.Repositories;
+
+namespace MvcLab.Tests
+{
+    [TestClass]
+    public class UserTests
+    {
+        [TestMethod]
+        public void RetreiveAnExistingUserFromTemporaryMemory()
+        {
+            //arrange
+            UserRepository userRepo = new UserRepository();
+            UserModel user = new UserModel()
+            {
+                Id = Guid.NewGuid(),
+                Firstname = "ivan",
+                Lastname = "prgomet",
+                Phone = "9238457",
+                Email = "ivan.prgomet@hotmail.com",
+                Street = "långgatan 74",
+                Username = "ivanprgomet",
+                Password = "ivano",
+                Country = "sweden",
+                City = "Lund",
+                DateRegistered = DateTime.Now,
+            };
+
+            UserRepository.Users.Add(user);
+
+            //act
+            var retrievedUser = userRepo.GetUser(user.Id);
+
+            //assert
+            Assert.AreEqual(user, retrievedUser);
+        }
+        [TestMethod]
+        public void CreatedUserGetsAddedToTemporaryMemory()
+        {
+            //arrange
+            UserRepository userRepo = new UserRepository();
+            UserModel user = new UserModel()
+            {
+                Id = Guid.NewGuid(),
+                Firstname = "ivan",
+                Lastname = "prgomet",
+                Phone = "9238457",
+                Email = "ivan.prgomet@hotmail.com",
+                Street = "långgatan 74",
+                Username = "ivanprgomet",
+                Password = "ivano",
+                Country = "sweden",
+                City = "Lund",
+                DateRegistered = DateTime.Now,
+            };
+
+            //Act
+            userRepo.CreateUser(user);
+
+            //Assert
+            if (UserRepository.Users.Contains(user))
+                Assert.IsTrue(true);
+            else
+                Assert.IsTrue(false);
+        }
+    }
+}
