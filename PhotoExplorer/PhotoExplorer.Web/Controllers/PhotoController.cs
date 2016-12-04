e51be6d3bd70a8a77ec58544f956da42fb07bf44
Ivan Prgomet <ivan_prgomet@hotmail.com>
2016-12-04 21:49:06 +0100

    added photos folder

---
+++ PhotoExplorer/PhotoExplorer.Web/Controllers/PhotoController.cs
@@ -0,0 +1,25 @@
+﻿using PhotoExplorer.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PhotoExplorer.Web.Controllers
+{
+    public class PhotoController : Controller
+    {
+        [HttpGet]
+        public ActionResult Details(int Id)
+        {
+            var retrievedPhoto = new PhotoModel();
+
+            using (PhotoExplorerDbContext cx = new PhotoExplorerDbContext())
+            {
+                retrievedPhoto = cx.Photos.FirstOrDefault(p => p.Id == Id);
+            }
+
+            return View("Details", retrievedPhoto);
+        }
+    }
+}
