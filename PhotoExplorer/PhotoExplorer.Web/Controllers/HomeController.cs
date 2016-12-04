263d5e63cc77c01c51ab71ebbc7a777c3e0cad7e
Ivan Prgomet <ivan_prgomet@hotmail.com>
2016-12-04 19:20:26 +0100

    added mvc template for login and identity

--- PhotoExplorer/PhotoExplorer.Web/Controllers/HomeController.cs
+++ PhotoExplorer/PhotoExplorer.Web/Controllers/HomeController.cs
@@ -1,49 +1,29 @@
-﻿using PhotoExplorer.Web.Models;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using System.Data.Entity;
 
 namespace PhotoExplorer.Web.Controllers
 {
     public class HomeController : Controller
     {
-        [HttpGet]
         public ActionResult Index()
         {
-            List<UserModel> usersFromDB = new List<UserModel>();
-
-            using (PhotoExplorerContext cx = new PhotoExplorerContext())
-            {
-                /*
-                    must include related properties because lazy loading is enabled for all 
-                    navigation properties, which means i load related properties only when they 
-                    are needed, which makes the application ultimately faster
-                */
-                usersFromDB = cx.Users
-                    .Include(u=>u.Albums
-                        .Select(a=>a.Photos))
-                    .ToList(); 
-            }
-
-            return View("Index",usersFromDB);
-        }
-        [HttpGet]
-        public ActionResult AboutUs()
-        {
             return View();
         }
-        [HttpGet]
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
         public ActionResult Contact()
         {
-            return View();
-        }
+            ViewBag.Message = "Your contact page.";
 
-        [HttpGet]
-        public ActionResult Error()
-        {
             return View();
         }
     }
