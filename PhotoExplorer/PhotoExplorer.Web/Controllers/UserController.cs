a8e7f2bd24ecf563a4e86b3d722c4158e0435b47
Ivan Prgomet <ivan_prgomet@hotmail.com>
2016-12-05 11:23:42 +0100

    display users

---
+++ PhotoExplorer/PhotoExplorer.Web/Controllers/UserController.cs
@@ -0,0 +1,25 @@
+﻿using PhotoExplorer.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PhotoExplorer.Web.Controllers
+{
+    public class UserController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            var users = new List<UserModel>();
+
+            using (PhotoExplorerContext cx = new PhotoExplorerContext())
+            {
+                users = cx.Users.ToList();
+            }
+
+            return View(users);
+        }
+    }
+}
