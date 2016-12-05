d07714150ed3a79943a68a7d67f2e6126abad77d
Ivan Prgomet <ivan_prgomet@hotmail.com>
2016-12-05 01:52:13 +0100

    album details

---
+++ PhotoExplorer/PhotoExplorer.Web/Controllers/AlbumController.cs
@@ -0,0 +1,29 @@
+﻿using PhotoExplorer.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+
+namespace PhotoExplorer.Web.Controllers
+{
+    public class AlbumController : Controller
+    {
+        [HttpGet]
+        public ActionResult Details(int id)
+        {
+            var albumToShow = new AlbumModel();
+
+            using (PhotoExplorerContext cx = new PhotoExplorerContext())
+            {
+                albumToShow = cx.Albums
+                    .Include(a=>a.Photos)
+                    .Include(a=>a.Comments)
+                    .FirstOrDefault(a => a.Id == id);
+            }
+
+            return View("Details",albumToShow);
+        }
+    }
+}
