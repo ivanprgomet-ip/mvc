a09f8c6a71445e5efcdbd3968f1632334b298e13
Ivan Prgomet <ivan_prgomet@hotmail.com>
2016-12-07 14:51:43 +0100

    view a photo from inside the album details view for the logged in user

--- PhotoExplorer/PhotoExplorer.Web/Areas/Account/Controllers/PhotoController.cs
+++ PhotoExplorer/PhotoExplorer.Web/Areas/Account/Controllers/PhotoController.cs
@@ -2,6 +2,7 @@
 using PhotoExplorer.Web.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -49,6 +50,79 @@ namespace PhotoExplorer.Web.Areas.Account.Controllers
             }
 
             return RedirectToAction("Index","Home");//redirects to the home index inside account area
+        }
+        [HttpPost]
+        public ActionResult Comment(int id, string txt_comment)
+        {
+            PhotoDetailsViewModel model = null;
+
+            using (PhotoExplorerEntities cx = new PhotoExplorerEntities())
+            {
+                #region get the photoentity commented on
+                PhotoEntityModel entity = cx.Photos
+                    .Where(p => p.Id == id)
+                    .Include(p => p.Comments)
+                    .FirstOrDefault();
+                #endregion
+
+                #region prepare a new comment for the photo
+                CommentEntityModel commentModel = new CommentEntityModel()
+                {
+                    DateCreated = DateTime.Now,
+                    Comment = txt_comment,
+                };
+                #endregion
+
+                entity.Comments.Add(commentModel);
+
+                cx.SaveChanges();
+
+                #region mapping entity to model
+                model = new PhotoDetailsViewModel()
+                {
+                    Name = entity.Name,
+                    Comments = entity.Comments,
+                    Album = entity.Album,
+                    DateCreated = entity.DateCreated,
+                    FileName = entity.FileName,
+                    Description = entity.Description,
+                    Id = entity.Id,
+                    User = entity.User,
+                };
+                #endregion
+            }
+
+            /*
+                note: only updating portion of the page by using partial view (with the NEW model)            
+            */
+            return PartialView("_PhotoComments", model);
+        }
+        public ActionResult Details(PhotoDetailsViewModel model)
+        {
+            #region retrieve photo to show
+
+            using (PhotoExplorerEntities cx = new PhotoExplorerEntities())
+            {
+                PhotoEntityModel entity = cx.Photos
+                    .Where(p => p.Id == model.Id)
+                    .Include(p => p.Comments)
+                    .FirstOrDefault();
+
+                model = new PhotoDetailsViewModel()
+                {
+                    Id = entity.Id,
+                    Name = entity.Name,
+                    FileName = entity.FileName,
+                    DateCreated = entity.DateCreated,
+                    Album = entity.Album,
+                    Comments = entity.Comments,
+                    Description = entity.Description,
+                    User = entity.User,
+                };
+            }
+            #endregion
+
+            return View("Details", model);
         }
     }
 }
