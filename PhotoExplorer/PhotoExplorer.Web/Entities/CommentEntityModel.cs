9105e356f6527e1943c206bfde28ef75aa4caf15
Ivan Prgomet <ivan_prgomet@hotmail.com>
2016-12-08 00:08:16 +0100

    fix small bugg

--- PhotoExplorer/PhotoExplorer.Web/Entities/CommentEntityModel.cs
+++ PhotoExplorer/PhotoExplorer.Web/Entities/CommentEntityModel.cs
@@ -16,5 +16,12 @@ namespace PhotoExplorer.Web.Entities
 
         public virtual PhotoEntityModel Photo { get; set; }
         public virtual AlbumEntityModel Album { get; set; }
+
+        public CommentEntityModel()
+        {
+            //initializing this makes us avoid the: The ObjectContext instance has been disposed and can no longer be used for operations that require a connection.
+            //when we make a comment, go out of details page , and come back into the photo details where we made the comment
+            User = new UserEntityModel();
+        }
     }
 }
