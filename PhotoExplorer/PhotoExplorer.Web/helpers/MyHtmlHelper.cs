0bd735af79e252744335a172687bd2130f5ad306
Ivan Prgomet <ivan_prgomet@hotmail.com>
2016-12-08 20:38:37 +0100

    added custom html helper just for the sake of it!

---
+++ PhotoExplorer/PhotoExplorer.Web/helpers/MyHtmlHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PhotoExplorer.Web.helpers
+{
+    public static class MyHtmlHelper
+    {
+        public static IHtmlString UpperCase(this HtmlHelper helper, string input)
+        {
+            return helper.Raw(input.ToString().ToUpper());
+        }
+    }
+}
