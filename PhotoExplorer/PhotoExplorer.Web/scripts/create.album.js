b9c9ef4275908ab781b72bbf6b2f8863d8495540
Ivan Prgomet <ivan_prgomet@hotmail.com>
2016-12-27 14:08:47 +0100

    creating new album using ajax

---
+++ PhotoExplorer/PhotoExplorer.Web/scripts/create.album.js
@@ -0,0 +1,23 @@
+﻿/// <reference path="jquery-3.1.1.js" />
+
+var form = $('form');//theres only one form on the page anyway
+
+form.on("submit", function (e) {//attach event handler using jquery
+    e.preventDefault();
+    //if (form.valid) {
+        $.ajax({
+            method: "POST",
+            url: "/Account/AlbumCreate",
+
+            data: new FormData(form[0]),
+            success: function (data) {
+                $("#create-msg").append("album successfully created");
+            },
+            error: function (e) {
+                $("#create-msg").append("something went soo wrong");
+            },
+            processData: false,
+            contentType: false
+        });
+    //}
+})
