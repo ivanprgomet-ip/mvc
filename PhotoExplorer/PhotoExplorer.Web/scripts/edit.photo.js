12c43dd1da78390f08bff439a62b2e5cb60e4654
Ivan Prgomet <ivan_prgomet@hotmail.com>
2016-12-29 10:21:32 +0100

    edit photo using ajax

---
+++ PhotoExplorer/PhotoExplorer.Web/scripts/edit.photo.js
@@ -0,0 +1,26 @@
+﻿/// <reference path="jquery-3.1.1.js" />
+
+var form = $('form');
+
+form.on('submit', function (e) {
+
+    e.preventDefault();
+
+    var formdata = new FormData(form[0]);
+
+    $.ajax({
+        url: "/Account/PhotoEdit",
+        method: "post",
+        data: formdata,
+        processData: false,
+        contentType: false,
+        success: function (data) {
+            console.log("edit suceeded");
+            window.location = $('#BackToDetails').attr('href');
+        },
+        error: function (data) {
+            console.log("Couldn't update image details");
+        }
+    });
+
+});
