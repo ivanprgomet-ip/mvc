d2608c3c3e3ca8ccbcd4ccb8287d239e140a2da1
Ivan Prgomet <ivan_prgomet@hotmail.com>
2016-12-29 11:07:43 +0100

    photos uploaded using ajax

---
+++ PhotoExplorer/PhotoExplorer.Web/scripts/create.photo.js
@@ -0,0 +1,32 @@
+﻿/// <reference path="jquery-3.1.1.js" />
+
+var form = $('form');
+var loader = $(".loader").hide();
+
+form.on('submit', function (e) {
+
+    e.preventDefault();
+
+    var formdata = new FormData(form[0]);
+
+    $.ajax({
+        url: "/Account/PhotoCreate",
+        method: "post",
+        data: formdata,
+        processData: false,
+        contentType: false,
+        success: function (data) {
+            $("#photocreate-msg").append("photo successfully uploaded");
+        },
+        error: function (data) {
+            $("#photocreate-msg").append("something went soo wrong");
+        },
+        beforeSend: function () {
+            loader.show();
+        },
+        complete: function () {
+            loader.hide();
+        }
+    });
+
+});
