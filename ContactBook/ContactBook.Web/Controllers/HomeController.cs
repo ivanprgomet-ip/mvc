4032ea098b6bedadb1e62d8e13b4d10a0e3f4b91
Ivan Prgomet <ivan_prgomet@hotmail.com>
2017-01-05 11:22:09 +0100

    contactbook done

--- ContactBook/ContactBook.Web/Controllers/HomeController.cs
+++ ContactBook/ContactBook.Web/Controllers/HomeController.cs
@@ -51,5 +51,41 @@ namespace ContactBook.Web.Controllers
 
             return PartialView("List",contactsDB);
         }
+
+        [HttpGet]
+        public ActionResult Details(Guid id)
+        {
+            ContactViewModel model = contactsDB.Where(c => c.Id == id).FirstOrDefault();
+
+            return View(model);
+        }
+
+        [HttpGet]
+        public ActionResult Edit(Guid id)
+        {
+            ContactViewModel retrievedModel = contactsDB.Where(c => c.Id == id).FirstOrDefault();
+
+            ContactEditViewModel model = new ContactEditViewModel()
+            {
+                Id = retrievedModel.Id,
+                Address = retrievedModel.Address,
+                Name = retrievedModel.Name,
+                Phone = retrievedModel.Phone,
+            };
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(ContactEditViewModel model)
+        {
+            ContactViewModel updateModel = contactsDB.Where(c => c.Id == model.Id).FirstOrDefault();
+
+            updateModel.Address = model.Address;
+            updateModel.Name = model.Name;
+            updateModel.Phone = model.Phone;
+            updateModel.Changed = DateTime.Now;
+
+            return RedirectToAction("Details",updateModel);
+        }
     }
 }
--- ContactBook/ContactBook.Web/Models/ContactBookViewModels.cs
+++ ContactBook/ContactBook.Web/Models/ContactBookViewModels.cs
@@ -13,6 +13,22 @@ namespace ContactBook.Web.Models
         public string Phone { get; set; }
         public string Address { get; set; }
         public DateTime Changed { get; set; }
+
+        public ContactViewModel()
+        {
+            Id = Guid.NewGuid();
+        }
+    }
+    public class ContactEditViewModel
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+        public ContactEditViewModel()
+        {
+            Id = Guid.NewGuid();
+        }
     }
     public class ContactListedViewModel
     {
